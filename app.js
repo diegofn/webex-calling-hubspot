ba0becae82da64b3647f92d1df9eb23976f90034
Diego Fernando Nieto <openbsdhacker@gmail.com>
2025-04-02 10:23:14 -0500

    Added express routes - Added Zoom Webhook and email notification

--- app.js
+++ app.js
@@ -9,18 +9,31 @@ const axios = require('axios');
 const { config } = require('dotenv');
 const app = express();
 const formatISO = require('date-fns/formatISO');
+const nodemailer = require('nodemailer');
+const { ICalEvent } = require('ical-generator');
+const ical = require('ical-generator').default;
+const qs = require('qs');
 require('dotenv').config({ path: '.env' });
+
+//
+// Map the routes
+//
+const hubspotRoute = require('./routes/Hubspot');
+const zoomRoute = require('./routes/Zoom');
 
 //
 // Encoding bodies support
 //
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/Hubspot', hubspotRoute);
+app.use('/Zoom', zoomRoute);
 
 //
 // Set the consts
 //
 const port = process.env.PORT || 3000;
+
 const HUBSPOT_API_URL = process.env.HUBSPOT_API_URL;
 const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY;
 const HUBSPOT_OWNER_ID = process.env.HUBSPOT_OWNER_ID;
@@ -43,185 +56,6 @@ const server = app.listen(port, function() {
 //
 app.use(express.static('public'));
 
-//
-// GET /Hubspot for Hubspot integration
-//
-app.get('/Hubspot', async function(req, res){
-    try {
-        if (req.query){
-
-            const { TenantID, InteractionID, DNIS, QueueID, AgentID, AgentName, ANI, QueueName } = req.query;
-            if (TenantID && InteractionID && DNIS && QueueID && AgentID && AgentName && ANI && QueueName) {
-                console.log ("Receive Popup Request");
-                console.log (`TenantID: ${TenantID}`);
-                console.log (`InteractionID: ${InteractionID}`);
-                console.log (`DNIS: ${DNIS}`);
-                console.log (`QueueID: ${QueueID}`);
-                console.log (`AgentID: ${AgentID}`);
-                console.log (`AgentName: ${AgentName}`);
-                console.log (`ANI: ${ANI}`);
-                console.log (`QueueName: ${QueueName}`);
-    
-                //
-                // Get the contact information from the Hubspot API
-                //
-                let contactId = await getContactIdByPhone(ANI) 
-                console.log("Contact ID: " + contactId);
-    
-                //
-                // Register the call in hubspot
-                // Redirect to the hubspot call URL
-                //
-                if (contactId){
-                    let callId = await createCallEngagement(ANI, DNIS, contactId, req.headers.host, InteractionID);
-                    console.log("Call ID: " + callId);
-                    res.redirect(`https://app.hubspot.com/contacts/${HUBSPOT_PORTAL_ID}/contact/${contactId}/?engagement=${callId}`);
-
-                } else {
-                    res.redirect(`https://app.hubspot.com/contacts/${HUBSPOT_PORTAL_ID}`);
-                }
-                    
-            }
-        }
-        else{
-            res.sendStatus(404);
-        }
-    }
-    catch (error) {
-        console.error("Error processing request:", error);
-        res.sendStatus(500).send("Internal Server Error");
-    }
-    
-});
-
-//
-// Get Hubspots Contact Id by phone
-//
-async function getContactIdByPhone(phone){
-    try{
-        //
-        // Create the data to search
-        //
-        let data = JSON.stringify({
-            "filterGroups": [
-            {
-                "filters": [
-                    {
-                    "propertyName": "phone",
-                    "operator": "CONTAINS_TOKEN",
-                    "value": "*" + phone
-                    }
-                ]
-            }
-        ]
-        });
-
-        //
-        // Create the request config
-        //
-        let config = {
-            method: 'post',
-            maxBodyLength: Infinity,
-            url: HUBSPOT_API_URL + '/crm/v3/objects/contacts/search',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + HUBSPOT_API_KEY
-            },
-            data: data
-        };
-
-        //
-        // Make the request
-        //
-        let response = await axios.request(config);
-        if (response.data.results[0].id)
-            return response.data.results[0].id;
-        else
-            return null;
-
-    } catch (error) {
-        console.error("Error fetching contact ID:", error);
-        return null;
-    }
-    
-}
-
-//
-// Create a Call in Hubspot
-//
-async function createCallEngagement(ANI, DNIS, contactId, host, interactionID){
-    try {
-        const CallDisposition = Object.freeze({
-            BUSY:           '9d9162e7-6cf3-4944-bf63-4dff82258764',
-            CONNECTED:      'f240bbac-87c9-4f6e-bf70-924b57d47db7',
-            LEFT_MESSAGE:   'a4c4c377-d246-4b32-a13b-75a56a4cd0ff',
-            LEFT_VOICEMAIL: 'b2cf5968-551e-4856-9783-52b3da59a7d0',
-            NO_ANSWER:      '73a0d17f-1163-4015-bdd5-ec830791da20',
-            WRONG_NUMBER:   '17b47fee-58de-441e-a44c-c6300d46f273'
-        });
-    
-        //
-        // Create the data to search
-        //
-        let date = new Date();
-        let isoDate = formatISO.formatISO(date);
-        let data = JSON.stringify({
-            "properties": {
-                "hs_timestamp": isoDate,
-                "hs_call_title": "Webex Calling Call",
-                "hubspot_owner_id": HUBSPOT_OWNER_ID,
-                "hs_call_body": `Enter your comments here ... <br/>Transcription link: https://${host}/Webex/Transcription?InteractionID=${interactionID}`,
-                "hs_call_direction": "INBOUND",
-                "hs_call_disposition": CallDisposition.CONNECTED,
-                "hs_call_duration": "3000",
-                "hs_call_from_number": ANI,
-                "hs_call_to_number": DNIS,
-                "hs_call_recording_url": `https://${host}/recording-not-available-es.mp3?InteractionID=${interactionID}`,
-                "hs_call_status": "IN_PROGRESS"
-              },
-              "associations": [
-                {
-                  "to": {
-                    "id": contactId
-                  },
-                  "types": [
-                    {
-                      "associationCategory": "HUBSPOT_DEFINED",
-                      "associationTypeId": 194
-                    }
-                  ]
-                }
-            ]
-        });
-    
-        //
-        // Create the request config
-        //
-        let config = {
-            method: 'post',
-            maxBodyLength: Infinity,
-            url: HUBSPOT_API_URL + '/crm/v3/objects/calls',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + HUBSPOT_API_KEY
-            },
-            data: data
-        };
-    
-        //
-        // Make the request
-        //
-        let response = await axios.request(config);
-        if (response.data.id)
-            return response.data.id;
-        else
-            return null;
-    }
-    catch (error) {
-        console.error("Error creating call engagement:", error);
-        return null;
-    }
-}
 
 
 //
---
+++ routes/Zoom.js
@@ -0,0 +1,209 @@
+const express = require('express');
+const axios = require('axios');
+const { config } = require('dotenv');
+
+const formatISO = require('date-fns/formatISO');
+const nodemailer = require('nodemailer');
+const ical = require('ical-generator').default;
+const qs = require('qs');
+require('dotenv').config({ path: '.env' });
+const MSGRAPH_CLIENT_ID = process.env.MSGRAPH_CLIENT_ID;
+const MSGRAPH_CLIENT_SECRET = process.env.MSGRAPH_CLIENT_SECRET;
+const MSGRAPH_TENANT_ID = process.env.MSGRAPH_TENANT_ID;
+
+const MSGRAPH_USER_URL = process.env.MSGRAPH_USER_URL;
+const MSGRAPH_EMAIL_USER = process.env.MSGRAPH_EMAIL_USER;
+
+const router = express.Router();
+
+//
+// POST / for Zoom Integration
+//
+router.post('/', async function(req, res){
+    try {
+        if (req.body){
+            const data = req.body;
+            
+            //
+            // New meeting event
+            //
+            console.log (`======== New Event: ${data.event} with UUID: ${data.payload.object.uuid}========`);
+            if (data.event == "meeting.created"){
+                console.log (`operator:  ${data.payload.operator}`);
+                console.log (`uuid:  ${data.payload.object.uuid}`);
+                console.log (`topic:  ${data.payload.object.topic}`);
+                console.log (`start_time:  ${data.payload.object.start_time}`);
+                console.log (`join_url:  ${data.payload.object.join_url}`);
+                console.log (`duration:  ${data.payload.object.duration}`);
+                console.log (`timezone:  ${data.payload.object.timezone}`);
+
+                //
+                // Request a new Microsoft Graph token
+                //
+                let token_data = qs.stringify({
+                    'grant_type': 'client_credentials',
+                    'client_id': MSGRAPH_CLIENT_ID,
+                    'client_secret': MSGRAPH_CLIENT_SECRET,
+                    'scope': 'https://graph.microsoft.com/.default'
+                });
+                const MSGRAPH_TOKEN_URL = 'https://login.microsoftonline.com/' + MSGRAPH_TENANT_ID + '/oauth2/v2.0/token';
+            
+                //
+                // Create the request config
+                //
+                let config = {
+                    method: 'post',
+                    maxBodyLength: Infinity,
+                    url: MSGRAPH_TOKEN_URL,
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    data: token_data
+                };
+            
+                //
+                // Make the request
+                //
+                let msgraph_token;
+                let response = await axios.request(config);
+                if (response.data.access_token)
+                    msgraph_token = response.data.access_token;
+                else
+                    return null;
+
+                //
+                // Create the event in Microsoft Graph
+                //
+                const endDateTime = new Date(data.payload.object.start_time);
+                endDateTime.setMinutes  (endDateTime.getMinutes() + data.payload.object.duration);
+                let event_data = JSON.stringify({
+                    "subject": data.payload.object.topic,
+                    "start": {
+                      "dateTime": data.payload.object.start_time,
+                      "timeZone": data.payload.object.timezone
+                    },
+                    "end": {
+                      "dateTime": endDateTime.toISOString(),
+                      "timeZone": data.payload.object.timezone
+                    },
+                    "location": {
+                      "displayName": data.payload.object.join_url,
+                      "locationUri": data.payload.object.join_url
+                    },
+                    "attendees": [
+                      {
+                        
+                      }
+                    ]
+                  });
+
+                //
+                // Create the request config
+                //
+                let event_config = {
+                    method: 'post',
+                    maxBodyLength: Infinity,
+                    url: MSGRAPH_USER_URL + MSGRAPH_EMAIL_USER + '/events',
+                    headers: { 
+                        'Content-Type': 'application/json', 
+                        'Authorization': 'Bearer ' + msgraph_token
+                    },
+                    data : event_data
+                };
+
+                //
+                // Make the request
+                //
+                let response_event = await axios.request(event_config);
+                if (response_event)
+                    return response_event.id;
+                else
+                    return null;
+                
+            }            
+        }
+        else{
+            res.sendStatus(404);
+        }
+    }
+    catch (error) {
+        console.error("Error processing request:", error);
+        res.sendStatus(500).send("Internal Server Error");
+    }
+    
+});
+
+//
+// Send the email with the .ics file
+//
+router.post('/SendMail', async function(req, res){
+
+    //
+    // Send the .ics to a email user
+    //
+    let transporter = nodemailer.createTransport({
+        host: "smtp.163.com",
+        port: "25",
+        
+        auth: {
+            user: process.env.NODEMAIL_USER,
+            pass: process.env.NODEMAIL_PASSWORD
+        }
+    });
+    
+    //
+    // Create the calendar invitation
+    //
+    const cal = ical({ name: data.payload.object.topic });
+    const startTime = new Date();
+    const endTime = new Date();
+    startTime.setHours(startTime.getHours() + 1);
+    endTime.setHours(startTime.getHours() + 2);
+
+    cal.createEvent({
+        start: startTime,
+        end: endTime,
+        summary: data.payload.object.topic,
+        description: "Zoom Meeting",
+        location: data.payload.object.join_url,
+        url: data.payload.object.join_url,
+        organizer: {
+            name: "Webex Zoom Webhook",
+            email: data.payload.operator
+        },
+    });
+    
+    //
+    // Mail options
+    //
+    var maillist = [
+        'mail1@gmail.com',
+        'mail2@gmail.com',
+    ];
+
+    let mailOptions = {
+        from: process.env.NODEMAIL_USER,
+        to: maillist,
+        subject: data.payload.object.topic,
+        text: "You have a new meeting invitation",
+        attachments: [
+            {
+                name: 'invite.ics',
+                type: 'text/calendar;method=REQUEST;name=\"invite.ics\"',
+                data: cal.toString(),
+            },
+        ],
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            res.status(500).send({
+                message: {
+                Error: "Could not sent email"
+                }
+        })
+        }
+    });  
+});
+
+module.exports = router;
